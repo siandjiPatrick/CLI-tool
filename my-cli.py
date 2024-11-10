47e02308483aa8dccf49b656d5a3ae3f1e6167c1
patrick Siandji <siandjipatrick@yahoo.fr>
2024-11-10 03:18:42 +0100

    add a new option salary from type integer which required multiple argument

--- my-cli.py
+++ my-cli.py
@@ -4,8 +4,14 @@ import click
 @click.command()
 #basic options
 @click.option("--name","-n", default="Musterman", help="Firstname description")
-def main(name):
-    print(f"Hello World, My name is {name}")
+
+#multiple Values : !!! for this option 2 Arguments are required
+@click.option("--favorites","-f", nargs=2, default=["Dance", "programing"], help="Your Favorites Activities")
+
+#Multiples values
+@click.option("--salary", "-s", type = int ,nargs=2, help="Your Monthly Salary")
+def main(name, favorites, salary):
+    print(f"Hello World, My name is {name} and my monthly salary is {sum(salary)}. These are my fovorites activities: {favorites}")
 
 
 
