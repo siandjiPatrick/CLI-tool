146d623c461a1622e51432c0e1fea7dfd2773711
patrick Siandji <siandjipatrick@yahoo.fr>
2024-11-10 04:36:05 +0100

    update script to do basic calculation

--- customcli.py
+++ customcli.py
@@ -4,11 +4,21 @@ import click
 @click.command()
 
 # work with argument
-@click.argument('name', default="patrick")
+@click.argument('number1', type=int )
+@click.argument('number2', type=int)
+@click.argument('method', default='add')
 
-def main(name):
-    click.echo("Hello {}".format(name))
-    
+def main(number1, number2, method):
+    if method == "add":
+        click.echo("{} + {} = {}".format(number1, number2,number1+number2))
+    elif method == "sub":
+        click.echo("{} - {} = {}".format(number1, number2,number1-number2))
+    elif method == "mul":
+        click.echo("{} * {} = {}".format(number1, number2,number1*number2))
+    elif method == "div":
+        click.echo("{} / {} = {}".format(number1, number2,number1/number2))
+    elif method == "mod":
+        click.echo("le reste de la division de {} par  {} est {}".format(number1, number2,number1%number2))
 
 
 if __name__ == "__main__":
