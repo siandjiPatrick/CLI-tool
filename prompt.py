d4b70c4276bdceb05b698e995f10a71881b7268b
patrick Siandji <siandjipatrick@yahoo.fr>
2024-11-10 05:19:59 +0100

    update script

--- prompt.py
+++ prompt.py
@@ -3,11 +3,11 @@ import click
 
 
 @click.command()
-@click.option("--name", default="patrick", help="Your Name Description")
+@click.option("--name", prompt=True, default="Siandji", help="Your Name Description")
 
 def main(name):
-    fname= click.prompt("Enter your Name")
-    click.echo(f" Your name is {fname}")
+    fname= click.prompt("Enter your firstname")
+    click.echo(f" Your name is {name} and your firtname is {fname}")
     
 if __name__=="__main__":
     main()
